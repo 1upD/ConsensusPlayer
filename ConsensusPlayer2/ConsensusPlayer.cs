4476be99029c7a837fde85c887ba8c82276d1599
Derek Dik <1upderek@gmail.com>
2015-10-23 22:26:09 -0400

    Initial Commit

---
+++ ConsensusPlayer2/ConsensusPlayer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsensusPlayer2
+{
+    public class ConsensusPlayer
+    {
+        public static int Main(string[] args)
+        {
+            ConsensusPlayer player = new ConsensusPlayer();
+            Board board = JsonConvert.DeserializeObject<Board>(args[0]);
+            return player.chooseMove(board);
+        }
+        public int chooseMove(Board board)
+        {
+            return 0;
+        }
+    }
+}
