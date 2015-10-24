773c286384f8b5db6a724748a034ec2fc9a01b99
Joel <jbs24@students.calvin.edu>
2015-10-24 11:08:45 -0400

    valid moves

--- ConsensusPlayer2/Board.cs
+++ ConsensusPlayer2/Board.cs
@@ -12,4 +12,25 @@ namespace ConsensusPlayer2
     public int height {get; set;}
     public string[] squares { get; set; }
     }
+
+    boolean validMoves(Board b)
+    {
+    boolean valids[64] = Enumerable.Repeat(false, 64).toArray();
+		for ( i =0; i < 64; i++){
+			if isTaken() valids[i] = false;
+			else
+				valids[i] = adjacentOpponent(i, direction)
+				
+		}
+	}
+
+	adjacentOpponent(Board b, location, direction, length=0) {
+		if (b[location+direction] is opponents
+			length++;
+			return adjacentOpponent(location+direction, direction, length)
+		else if is ours
+			if length >0
+				return true
+		return false
+	}
 }
