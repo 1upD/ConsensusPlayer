a10c7a7dba58b11f22fffbd4b297ec876059640f
Derek Dik <1upderek@gmail.com>
2015-10-23 23:33:20 -0400

    ValidMoves

--- ConsensusPlayer2/Board.cs
+++ ConsensusPlayer2/Board.cs
@@ -8,35 +8,48 @@ namespace ConsensusPlayer2
 {
     public class Board
     {
-    public int width { get; set; }
-    public int height {get; set;}
-    public string[] squares { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public string[] squares { get; set; }
 
-    public int[] ValidMoves()
-    {
-        return null;
+        public bool[] ValidMoves(string player)
+        {
+            int[] directions = { -9, -8, -7, -1, 1, 7, 8, 9 };
+            bool[] valids = Enumerable.Repeat(false, 64).ToArray();
+            for (int i = 0; i < 64; i++)
+            {
+                if (isTaken(i))
+                {
+                    valids[i] = false;
+                }
+                else
+                {
+                    foreach (int direction in directions)
+                    {
+                        valids[i] = adjacentOpponent(i, direction, player);
+                    }
+                }
+            }
+            return valids;
+        }
+
+        private bool isTaken(int i)
+        {
+            return squares[i] != "-";
+        }
+
+        private bool adjacentOpponent(int location, int direction, string player)
+        {
+            if (squares[location + direction] == "-")
+            {
+                return false;
+            }
+            if (squares[location + direction] == player)
+            {
+                return true;
+            }
+            return adjacentOpponent(location + direction, direction, player);
+        }
+
     }
-
-    }
-
-    boolean validMoves(Board b)
-    {
-    boolean valids[64] = Enumerable.Repeat(false, 64).toArray();
-		for ( i =0; i < 64; i++){
-			if isTaken() valids[i] = false;
-			else
-				valids[i] = adjacentOpponent(i, direction)
-				
-		}
-	}
-
-	adjacentOpponent(Board b, location, direction, length=0) {
-		if (b[location+direction] is opponents
-			length++;
-			return adjacentOpponent(location+direction, direction, length)
-		else if is ours
-			if length >0
-				return true
-		return false
-	}
 }
